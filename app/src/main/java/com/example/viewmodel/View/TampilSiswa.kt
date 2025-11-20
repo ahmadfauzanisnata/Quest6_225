bdf6577ca348fbca34fe1b3c3fd42ace3fa993ab
ahmad fauzan isnata <160833065+ahmadfauzanisnata@users.noreply.github.com>
2025-11-20 11:31:50 +0700

    menyiapkan data siswa untuk ditampilkan menggunakan List of Pair

--- app/src/main/java/com/example/viewmodel/View/TampilSiswa.kt
+++ app/src/main/java/com/example/viewmodel/View/TampilSiswa.kt
@@ -34,4 +34,11 @@ fun TampilSiswa(
     //edit 1: parameter statusUISiswa
     statusUISiswa: Siswa,
     onBackButtonClicked: () -> Unit,
-)
+) {
+    //edit 2: items Pair
+    val items = listOf(
+        Pair(first = stringResource("Nama Lengkap"), second = statusUISiswa.nama),
+        Pair(first = stringResource(id = "Jenis Kelamin"), second = statusUISiswa.gender),
+        Pair(first = stringResource(id = "Alamat"), second = statusUISiswa.alamat),
+    )
+
