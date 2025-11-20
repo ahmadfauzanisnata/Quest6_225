b5d535a40b2c828ff3bb501cb842ac223f5c3cca
ahmad fauzan isnata <160833065+ahmadfauzanisnata@users.noreply.github.com>
2025-11-20 11:31:20 +0700

    mendefinisikan Composable TampilSiswa dengan parameter awal

---
+++ app/src/main/java/com/example/viewmodel/View/TampilSiswa.kt
@@ -0,0 +1,37 @@
+package com.example.viewmodel.View
+
+import android.widget.Button
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.viewmodel.R
+
+private val Siswa.alamat: Any
+private val Siswa.nama: Any
+private val Siswa.gender: Any
+
+@OptIn(markerClass = ExperimentalMaterial3Api::class)
+@Composable
+fun TampilSiswa(
+    //edit 1: parameter statusUISiswa
+    statusUISiswa: Siswa,
+    onBackButtonClicked: () -> Unit,
+)
